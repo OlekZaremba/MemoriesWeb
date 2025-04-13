b7a6501700e80e1fed19a237e88a1ecc342c1ee4
jakubbonczyk <jakubbonczyk@icloud.com>
2025-04-14 01:08:14 +0200

    Front changes

--- MemoriesFront/Memories/src/app/homepage/homepage.component.ts
+++ MemoriesFront/Memories/src/app/homepage/homepage.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import {MenuNavbarComponent} from '../menu-navbar/menu-navbar.component';
+import {LeftSidebarComponent} from '../left-sidebar/left-sidebar.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
-    MenuNavbarComponent
+    MenuNavbarComponent,
+    LeftSidebarComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
--- MemoriesFront/Memories/src/app/menu-navbar/menu-navbar.component.ts
+++ MemoriesFront/Memories/src/app/menu-navbar/menu-navbar.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'app-menu-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './menu-navbar.component.html',
   styleUrl: './menu-navbar.component.css'
 })
