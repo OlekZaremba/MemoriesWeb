d1b26080deab8251b536a24c63a4848b260c6c0c
jakubbonczyk <jakubbonczyk@icloud.com>
2025-04-19 16:33:37 +0200

    Edited homepage front

--- MemoriesFront/Memories/src/app/homepage/homepage.component.ts
+++ MemoriesFront/Memories/src/app/homepage/homepage.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import {MenuNavbarComponent} from '../menu-navbar/menu-navbar.component';
 import {LeftSidebarComponent} from '../left-sidebar/left-sidebar.component';
+import {HomeComponent} from '../home/home.component';
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
   imports: [
     MenuNavbarComponent,
-    LeftSidebarComponent
+    LeftSidebarComponent,
+    HomeComponent
   ],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
