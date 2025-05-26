508c16f3ff2374caad49f4539295b4c089733f91
Shemyhaza <byandiaq@gmail.com>
2025-05-26 20:48:02 +0200

    ukrywanie przycisków i widoków ocen w zależności od roli użytkownika

--- MemoriesFront/Memories/src/app/grades/grades.component.ts
+++ MemoriesFront/Memories/src/app/grades/grades.component.ts
@@ -1,17 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-grades',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './grades.component.html',
   styleUrl: './grades.component.css'
 })
-export class GradesComponent {
+export class GradesComponent implements OnInit {
   @Output() navigateTo = new EventEmitter<string>();
+  userRole: string | null = null;
+
+  ngOnInit(): void {
+    this.userRole = sessionStorage.getItem('userRole');
+  }
 
   goToGradeView() {
-    console.log('klik działa!');
     this.navigateTo.emit('grade-view');
   }
 
@@ -22,5 +27,4 @@ export class GradesComponent {
   goToGroupGrades() {
     this.navigateTo.emit('group-grades');
   }
-
 }
