86768e2c7ab7593306e6c655005912dc7b9f2f2a
Adrian Bandiak <byandiaq@gmail.com>
2025-05-26 13:34:20 +0200

    Dodanie obsługi pola email oraz konwersji enum Role do stringa w JSON

--- MemoriesBack/MemoriesBack/MemoriesBack/DTO/RegisterUserRequest.cs
+++ MemoriesBack/MemoriesBack/MemoriesBack/DTO/RegisterUserRequest.cs
@@ -8,6 +8,7 @@ namespace MemoriesBack.DTO
         public string Surname { get; set; }
         public string Login { get; set; }
         public string Password { get; set; }
+        public string Email { get; set; }
         public User.Role Role { get; set; }
         public int GroupId { get; set; }
 
