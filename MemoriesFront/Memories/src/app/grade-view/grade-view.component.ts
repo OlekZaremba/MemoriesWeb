17a702b80e22a63dc6a3c826dfaa5c6de9530830
Shemyhaza <byandiaq@gmail.com>
2025-05-28 17:44:57 +0200

    push

--- MemoriesFront/Memories/src/app/grade-view/grade-view.component.ts
+++ MemoriesFront/Memories/src/app/grade-view/grade-view.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
+import { environment } from '../../environments/environment';
+
+interface Grade {
+  type: string;
+  issueDate: string;
+  grade: number;
+  description: string;
+}
 
 @Component({
   selector: 'app-grade-view',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './grade-view.component.html',
   styleUrl: './grade-view.component.css'
 })
-export class GradeViewComponent {
+export class GradeViewComponent implements OnChanges {
+  @Input() subjectId!: number;
+  grades: Grade[] = [];
 
+  constructor(private http: HttpClient) {}
+
+  ngOnChanges(): void {
+    const studentId = sessionStorage.getItem('userId');
+    if (studentId && this.subjectId) {
+      this.http.get<Grade[]>(`${environment.apiUrl}/grades/student/${studentId}/subject/${this.subjectId}`)
+        .subscribe({
+          next: (data) => this.grades = data,
+          error: (err) => console.error('Błąd pobierania ocen przedmiotu:', err)
+        });
+    }
+  }
 }
