6d377459fc786ff1fe1bf3f2206c749f322fcaa5
OlekZaremba <sarutern.gra@gmail.com>
2025-05-28 18:04:36 +0200

    Changed so classes doesn't show too students and teachers.

--- MemoriesFront/Memories/src/app/left-sidebar/left-sidebar.component.ts
+++ MemoriesFront/Memories/src/app/left-sidebar/left-sidebar.component.ts
@@ -1,20 +1,27 @@
-import {Component, EventEmitter, Output} from '@angular/core';
-import {RouterLink} from '@angular/router';
-import {LucideAngularModule} from 'lucide-angular';
+import { Component, EventEmitter, Output, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { LucideAngularModule } from 'lucide-angular';
+import {NgIf} from '@angular/common';
 
 @Component({
   selector: 'app-left-sidebar',
   standalone: true,
   imports: [
     RouterLink,
-    LucideAngularModule
+    LucideAngularModule,
+    NgIf
   ],
   templateUrl: './left-sidebar.component.html',
   styleUrl: './left-sidebar.component.css'
 })
-export class LeftSidebarComponent {
+export class LeftSidebarComponent implements OnInit {
   @Output() viewChange = new EventEmitter<string>();
   activeButton: string = 'glowna';
+  userRole: string | null = null;
+
+  ngOnInit(): void {
+    this.userRole = sessionStorage.getItem('userRole');
+  }
 
   setActive(view: string) {
     if (this.activeButton !== view) {
@@ -24,6 +31,4 @@ export class LeftSidebarComponent {
       this.activeButton = view;
     }
   }
-
 }
-
